6f809fd82baae2a64e80e93b1cca832fed61c255
KatieC97 <katiecox497@gmail.com>
2025-05-05 18:25:35 +0100

    Splash css addition

--- src/pages/Splash.js
+++ src/pages/Splash.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import logo from "../assets/noto-logo.png";
-import "../App.css";
+import "./Splash.css";
 
 function Splash() {
   const navigate = useNavigate();
